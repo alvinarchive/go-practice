5e2c61a51a9d2ba96858a9b683c31032e59b96d3
alvintanoto <alvintanotodev@gmail.com>
2022-05-20 15:55:06 +0700

    add api validator

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"movie.alvintanoto.id/internal/validator"
+)
 
 // change key in json object to json snakecase
 // to control the visibility of data:
@@ -14,4 +18,22 @@ type Movie struct {
 	Runtime   Runtime   `json:"runtime"` // use runtime type instead of int32
 	Genres    []string  `json:"genres"`
 	Version   int32     `json:"version"` // incremented every time movie info got updated
+}
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year > 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be positive integer")
+
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must at least contains 1 genres")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contains more than 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contains duplicate values")
+
 }
