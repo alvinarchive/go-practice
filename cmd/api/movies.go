158c67ddf13fa035719e751486109002183d8610
alvintanoto <alvintanotodev@gmail.com>
2022-05-26 22:10:29 +0700

    fix bug

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -55,7 +55,6 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
-	fmt.Fprintf(w, "%+v\n", input)
 }
 
 // Add showMovieHandler
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -48,7 +48,7 @@ type MovieModel struct {
 func (m MovieModel) Insert(movie *Movie) error {
 	query := `
 	INSERT INTO movies (title, year, runtime, genres) 
-	VALUES ($1, %2, %3, %4)
+	VALUES ($1, $2, $3, $4)
 	RETURNING id, created_at, version
 	`
 
